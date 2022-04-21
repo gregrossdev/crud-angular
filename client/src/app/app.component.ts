98de96b570ad1ba262cd68f66b3bb0e5285a8588
gregrossdev <gregross.dev@gmail.com>
2022-04-21 14:53:32 -0400

    request/response

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-crud';
+  constructor(private userService: UsersService) {}
+
+  ngOnInit() {
+    this.userService.getAllData().subscribe((data) => {
+      console.log(data);
+    });
+  }
 
   submitData(value: any) {
-    console.log(value)
+    let body = {
+      name: value.name,
+      age: value.age,
+    };
+    this.userService.postData(body).subscribe((response) => {
+      console.log(response);
+    });
+  }
+
+  updateData(value: any) {
+    let body = {
+      name: value.name,
+      age: value.age,
+    };
+
+    this.userService
+      .updateData(body, `62619f55a165224ebadd9a16`)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  delete() {
+    this.userService
+      .deleteData(`6261a613a165224ebadd9a2d`)
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,6 +15,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
